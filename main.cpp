a369ca81367b8c3c713b784545c20dd20f74f54c
Borisov Vadim <borissowadim@mail.ru>
2018-01-18 00:38:15 +0100

    work correctly with big numbers

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 //				 c++ -std=c++0x main.cpp -o main.exe
 
 #include <iostream>   // pour pouvoir utiliser cin, cout, endl
-#include <string> 	  // pour facilement convertir les entiers et les reels en chaines des caractes (to_string())
-#include <math.h>  	  // precision des reels a 3 chiffres significatifs		
+#include <string> 	  // pour facilement convertir les entiers et les reels en chaines de caracteres (to_string())
 using namespace std;  // pour éviter de devoir écrire std::cin, std::cout, std::endl
 
 /*---------------declarations------------*/
@@ -25,7 +24,6 @@ struct t_vecteur {
 /*------------procedures--------------*/
 
 // initialisation d'un vecteur
-
 void initialiser(t_vecteur &v, int dim, double def){
 	while (dim <= 0) {
 		cout << "entrez dimension positive! :" << endl;
@@ -37,7 +35,6 @@ void initialiser(t_vecteur &v, int dim, double def){
 }
 
 // vider un vecteur
-
 void vider(t_vecteur &v){
 	t_coord *temp;
 	temp = v.tete;
@@ -46,11 +43,9 @@ void vider(t_vecteur &v){
 		delete v.tete;
 		v.tete = temp;
 	}
-	
 }
 
 //modifier vecteur
-
 void modifier(t_vecteur &v, int ind, double val){
 	// coherence d`un indice
 	while (ind > v.dimension || ind < 1){
@@ -68,48 +63,45 @@ void modifier(t_vecteur &v, int ind, double val){
 			(nouveau)->suiv = nullptr;    
 			v.tete = nouveau;
 		}
-
 		else{							// cas d`un chainage pas vide
 			t_coord *temp;
 			temp = v.tete; 
-
 
 			// cas d`un seul maillon:
 			if ((temp)->suiv == nullptr){
 				if ((temp)->indice == ind){
 					(temp)->valeur = val;
-					cout << "change" << endl;
 				}
 				else if ((temp)->indice > ind){
 					t_coord *nouveau;
 					nouveau = new(t_coord);
-					(*nouveau).valeur = val;    
-					(*nouveau).indice = ind;
-					(*nouveau).suiv = temp;
+					(nouveau)->valeur = val;    
+					(nouveau)->indice = ind;
+					(nouveau)->suiv = v.tete;
 					v.tete = nouveau;
+					
 				}
 				else{
 					t_coord *nouveau_fin;
 					nouveau_fin = new(t_coord);
-					(*nouveau_fin).valeur = val;    
-					(*nouveau_fin).indice = ind;
-					(*nouveau_fin).suiv = nullptr; 
+					(nouveau_fin)->valeur = val;    
+					(nouveau_fin)->indice = ind;
+					(nouveau_fin)->suiv = nullptr; 
 					(temp)->suiv = nouveau_fin;
 				}
 			}
 
-
 			// cas d`un chainage contenant au moins 2 maillons:
 			else{
 				t_coord *precedent;
-
-				while ((temp)->suiv != nullptr && (temp)->indice != ind && (temp)->indice <= ind ){
+				while ((temp)->suiv != nullptr && (temp)->indice < ind ){
 				// condition d`arret: on trouve le maillon avec notre indice ou l`indice est plus grand que l`indice indique ou il n`y a pas 
 				// de maillon avec indice indique
 					precedent = temp;
 					temp = (temp)->suiv;
 				}
 				
+				// le cas de dernier maillon
 				if ((temp)->suiv == nullptr){
 					if ((temp)->indice == ind){
 						(temp)->valeur = val;
@@ -117,18 +109,17 @@ void modifier(t_vecteur &v, int ind, double val){
 					else if((temp)->indice > ind){    
 						t_coord *nouveau;
 						nouveau = new(t_coord);
-						(*nouveau).valeur = val;    
-						(*nouveau).indice = ind;
-						(*nouveau).suiv = temp;
-						delete temp;
-						(*precedent).suiv = nouveau;
+						(nouveau)->valeur = val;    
+						(nouveau)->indice = ind;
+						(nouveau)->suiv = temp;
+						(precedent)->suiv = nouveau;
 					}
 					else {
 						t_coord *nouveau_fin;
 						nouveau_fin = new(t_coord);
-						(*nouveau_fin).valeur = val;    
-						(*nouveau_fin).indice = ind;
-						(*nouveau_fin).suiv = nullptr; 
+						(nouveau_fin)->valeur = val;    
+						(nouveau_fin)->indice = ind;
+						(nouveau_fin)->suiv = nullptr; 
 						(temp)->suiv = nouveau_fin;
 					}
 				}
@@ -137,27 +128,23 @@ void modifier(t_vecteur &v, int ind, double val){
 					(temp)->valeur = val;
 				}
 
+				// le cas inserer maillon avant
 				else if((temp)->indice > ind){
 					t_coord *nouveau;
 					nouveau = new(t_coord);
-					(*nouveau).valeur = val;    
-					(*nouveau).indice = ind;
-					(*nouveau).suiv = temp;
+					(nouveau)->valeur = val;    
+					(nouveau)->indice = ind;
+					(nouveau)->suiv = temp;
 					if (temp == v.tete){
 						v.tete = nouveau;
 					}
 					else {
-						temp = nouveau;
+						(precedent)->suiv = nouveau;
 					}
 				}
-			}
-			
+			}	
 		}
-		
 	}
-
-
-
 
 
 
@@ -166,15 +153,12 @@ void modifier(t_vecteur &v, int ind, double val){
 	else {    
 		t_coord *temp;
 		temp = v.tete;
-
 		if (v.tete == nullptr){
 			// on fait rien car si la chainage des chiffres significatifs est vide 
 			//ca veut dire qu`on a que nos valeurs par default et la valeur default saisie
 			//par utilisateur ne faut pas entrer dans notre chainage des chiffres signif 
 			//par definition
 		}
-
-		
 
 		//cas d`un seul maillon:
 		else if ((temp)->suiv == nullptr){
@@ -187,15 +171,10 @@ void modifier(t_vecteur &v, int ind, double val){
 			}
 		}
 
-
-
-
-
 		// cas d`un chainage contenant au moins 2 maillons:
 		else{
 			t_coord *precedent, *suivant;
-			 
-			while ((temp)->suiv != nullptr && (temp)->indice != ind && (temp)->indice <= ind){
+			while ((temp)->suiv != nullptr && (temp)->indice < ind){
 				precedent = temp;
 				temp = (temp)->suiv;
 			}
@@ -204,9 +183,6 @@ void modifier(t_vecteur &v, int ind, double val){
 				if ((temp)->indice == ind){
 					(precedent)->suiv = nullptr;
 					delete temp;
-				}
-				else{
-					//rien faire, car on stock pas les valeurs par default dans le chainage
 				}
 			}
 
@@ -221,10 +197,6 @@ void modifier(t_vecteur &v, int ind, double val){
 					delete temp;
 				}
 			}
-
-			else if((temp)->indice > ind){
-				//rien faire
-			}
 		}
 	}
 }
@@ -235,8 +207,7 @@ void modifier(t_vecteur &v, int ind, double val){
 void saisir(t_vecteur &v){
 	bool fin = false;
 	string rep;
-	int cpt = 1;
-	while (fin == false && cpt <= v.dimension){
+	while (fin == false){
 		int ind;
 		double val;
 		cout << "----------saisie d`une valeur--------" << endl;
@@ -254,13 +225,10 @@ void saisir(t_vecteur &v){
 			cin >> val;
 		}
 		modifier(v, ind, val);
-		cpt++;
-		if (cpt <= v.dimension){
-			cout << "Voulez-vous saisir d`autre valeur? (o/n): " << endl;
-			cin >> rep;
-			if (rep == "n"){
-				fin = true;
-			}
+		cout << "Voulez-vous saisir d`autre valeur? (o/n): " << endl;
+		cin >> rep;
+		if (rep == "n"){
+			fin = true;
 		}
 	}
 }
@@ -269,8 +237,6 @@ void afficher(t_vecteur v, bool complet){
 	string seq;
 	t_coord *temp;
 	temp = v.tete;
-	string indice;
-	string valeur;
 	if (complet){
 		cout << "" << endl;
 		cout << "**************affichage complet d`un vecteur creux**************" << endl;
@@ -278,9 +244,9 @@ void afficher(t_vecteur v, bool complet){
 
 		// on evite la virguille a la fin d`un affichage
 		if (temp != nullptr){
-				if ((*temp).indice == 1){
-					seq += to_string((*temp).valeur);
-					temp = (*temp).suiv;
+				if ((temp)->indice == 1){
+					seq += to_string((temp)->valeur);
+					temp = (temp)->suiv;
 				}
 				else {
 					seq += to_string(v.defaut);
@@ -292,9 +258,9 @@ void afficher(t_vecteur v, bool complet){
 
 		for (int i = 1; i < v.dimension; i++){
 			if (temp != nullptr){
-				if ((*temp).indice == i+1){
-					seq +=  ", " + to_string((*temp).valeur);
-					temp = (*temp).suiv;
+				if ((temp)->indice == i+1){
+					seq +=  ", " + to_string((temp)->valeur);
+					temp = (temp)->suiv;
 				}
 				else {
 					seq += ", " + to_string(v.defaut);
@@ -311,20 +277,27 @@ void afficher(t_vecteur v, bool complet){
 	else {
 		cout << "" << endl;
 		cout << "**************affichage des valeurs significatifs**************" << endl;
-		cout << " " << endl;
-
-		// on affiche les paires des valeurs significatifs (indice:valeur)
-		while((temp)->suiv != nullptr){
-			seq = seq + to_string((*temp).indice) + ":" + to_string((*temp).valeur) + ", ";
-			temp = (*temp).suiv;
+		cout << "" << endl;
+		if (temp == nullptr){		// le cas si on a vide le vecteur
+			cout << "[]" << endl;
 		}
-		seq += to_string((*temp).indice) + ":" + to_string((*temp).valeur);
-		cout << "[" << seq << "]" << endl;
+		else {
+			// on affiche les paires des valeurs significatifs (indice:valeur)
+			while((temp)->suiv != nullptr){
+				seq = seq + to_string((temp)->indice) + ":" + to_string((temp)->valeur) + ", ";
+				temp = (temp)->suiv;
+			}
+			seq += to_string((temp)->indice) + ":" + to_string((temp)->valeur);
+			cout << "[" << seq << "]" << endl;
+		}
+		
 	}
 }
 
 
 /*------------fonctions--------------*/
+
+// calculer la somme des 2 vecteurs
 t_vecteur somme(t_vecteur v1, t_vecteur v2){
 	t_vecteur v3;
 	if (v1.dimension != v2.dimension){
@@ -338,32 +311,30 @@ t_vecteur somme(t_vecteur v1, t_vecteur v2){
 		t_coord *temp1, *temp2;
 		temp1 = v1.tete;
 		temp2 = v2.tete;
-		for (int i = 0; i < v3.dimension; i++){
+		int i = 0;
+		while (temp1 != nullptr || temp2 != nullptr){
 			if (temp1 != nullptr && temp2 != nullptr){
 				if ((temp1)->indice == i+1){
-					// cas: chiffre signif1 + chiffre signif2
+					// 1 cas: chiffre signif1 + chiffre signif2
 					if ((temp2)->indice == i+1){
 						modifier(v3, i+1, (temp1)->valeur + (temp2)->valeur);
 						temp1 = (temp1)->suiv;
 						temp2 = (temp2)->suiv;
 					}
 
-					// cas: chiffre signif1 + def2
+					// 2 cas: chiffre signif1 + def2
 					else{
 						modifier(v3, i+1, (temp1)->valeur + v2.defaut);
 						temp1 = (temp1)->suiv;
 					}
 				}
 
-					// cas: def1 + chiffre signif2
+					// 3 cas: def1 + chiffre signif2
 				else if ((temp2)->indice == i+1){
 					modifier(v3, i+1, v1.defaut + (temp2)->valeur);
 					temp2 = (temp2)->suiv;
 				}
-					// cas: de1+def2
-				else {
-					// rien faire
-				}
+	
 			}
 			else if (temp1 != nullptr || temp2 != nullptr){
 				if (temp1 != nullptr){
@@ -379,33 +350,93 @@ t_vecteur somme(t_vecteur v1, t_vecteur v2){
 					}
 				}
 			}
-			else {
-			}
+			i++;
 		}
 		return v3;
 	}	// fin else
 }
 
 
+// calculer le produit vectoriel
+double produit(t_vecteur v1, t_vecteur v2){
+	double prodScal = 0;
+	if (v1.dimension != v2.dimension){
+		cout << "les dimensions des 2 vecteurs doivent etre egaux" << endl;
+		return prodScal = -1;
+	}
+	else {
+		int dim = v1.dimension;
+		t_coord *temp1, *temp2;
+		temp1 = v1.tete;
+		temp2 = v2.tete;
+		if (temp1 == nullptr && temp2 == nullptr){		// le cas avec les vecteurs seulement avec les valeurs defauts
+			prodScal = (v1.defaut * v2.defaut)*dim;
+			return prodScal;
+		}
+		else{
+			int i = 0;
+			while (temp1 != nullptr || temp2 != nullptr){
+				if (temp1 != nullptr && temp2 != nullptr){
+					if ((temp1)->indice == i+1){
+						if ((temp2)->indice == i+1){    // 1 cas: chiffre signif1 + chiffre signif2
+							prodScal += (temp1)->valeur * (temp2)->valeur;
+							temp1 = (temp1)->suiv;
+							temp2 = (temp2)->suiv;
+						}
+						else{							// 2 cas: chiffre signif1 + def2
+							prodScal += (temp1)->valeur * v2.defaut;
+							temp1 = (temp1)->suiv;
+						}
+					}
+					else if ((temp2)->indice == i+1){	// 3 cas: def1 + chiffre signif2
+						prodScal += v1.defaut * (temp2)->valeur;
+						temp2 = (temp2)->suiv;
+					}
+				}
+				
+				else if (temp1 != nullptr) {
+					if ((temp1)->indice == i+1){
+						prodScal += (temp1)->valeur * v2.defaut;
+						temp1 = (temp1)->suiv;
+					}
+				}
+				else if (temp2 != nullptr){
+					if ((temp2)->indice == i+1){
+						prodScal += v1.defaut * (temp2)->valeur;
+						temp2 = (temp2)->suiv;
+					}
+				}
+				i++;
+			}
+			if (i < dim){
+				prodScal += (v1.defaut * v2.defaut)*(dim - i);
+			}
+			return prodScal;
+		}	
+	}	// fin else
+}
+
+
 /**************** fonction principale *************************/
 int main(){
-	t_vecteur vect1;
-	t_vecteur vect2;
-	t_vecteur vect3;
-	initialiser(vect1,4,7);
-	initialiser(vect2,4,5);
-	saisir(vect1);
-	saisir(vect2);
-	afficher(vect1, true);
-	afficher(vect2, true);
-	vect3 = somme(vect1,vect2);
-	afficher(vect3, true);
-/*
-	t_vecteur vect;
-	initialiser(vect,4,7);
-	saisir(vect);
-	afficher(vect, true);
-	//modifier(vect, 2, 7);
-	afficher(vect, false);*/
+	t_vecteur vect, vect2, vect3;
+	initialiser(vect,1000000,5);
+	initialiser(vect2, 1000000, 15);
+	modifier(vect, 19, 8);
+	modifier(vect, 17, 10);
+	modifier(vect, 4867, 10);
+	modifier(vect, 3123, 10);
+	modifier(vect, 45, 8);
+	modifier(vect, 52, 8);
+	modifier(vect, 987, 8);		
+	modifier(vect, 6582, 8);
+	vect3 = somme(vect,vect2);
+	afficher(vect, false);
+	afficher(vect3, false);
+	double prod = produit(vect, vect2);
+	cout << "produit scalaire est: " << prod << endl;
+	vider(vect);
+	afficher(vect, false);
 	return 0;
+
 }
